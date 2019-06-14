86e59f6db0f9155aca6fbebd3185b05519e9a2f4
dayton brock <dayton.brock@gmail.com>
2019-06-14 14:44:04 -0500

    bring in redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// REDUX
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+
+const feelingReducer = (state = 0, action) => {
+
+    return state;
+}
+
+const store = createStore(
+    combineReducers({
+        feelingReducer,
+    }),
+    applyMiddleware( logger ),
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
