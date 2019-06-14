93d0a6a4ce1ec8bb2e35d81b0cc7dcf7de2d58b4
dayton brock <dayton.brock@gmail.com>
2019-06-14 16:23:23 -0500

    build out reducers

--- src/index.js
+++ src/index.js
@@ -9,14 +9,40 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { logger } from 'redux-logger';
 
-const feelingReducer = (state = 0, action) => {
+const feelingsFeedback = (state = 0, action) => {
+    if (action.type === 'FEELINGS_FEEDBACK') {
+        return action.payload;
+    }
+    return state;
+}
 
+const understandingFeedback = (state = 0, action) => {
+    if (action.type === 'UNDERSTANDING_FEEDBACK') {
+        return action.payload;
+    }
+    return state;
+}
+
+const supportFeedback = (state = 0, action) => {
+    if (action.type === 'SUPPORT_FEEDBACK') {
+        return action.payload;
+    }
+    return state;
+}
+
+const commentsFeedback = (state = '', action) => {
+    if (action.type === 'COMMENTS_FEEDBACK') {
+        return action.payload;
+    }
     return state;
 }
 
 const store = createStore(
     combineReducers({
-        feelingReducer,
+        feelingsFeedback,
+        understandingFeedback,
+        supportFeedback,
+        commentsFeedback
     }),
     applyMiddleware( logger ),
 );
